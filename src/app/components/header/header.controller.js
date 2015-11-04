907e3ddb0808f91ad0a72983607ad5f2398ca1c3
Paul Joannon <hello@pauljoannon.com>
2015-11-04 11:47:50 +0100

    disable header animation

--- src/app/components/header/header.controller.js
+++ src/app/components/header/header.controller.js
@@ -9,20 +9,20 @@ class HeaderController {
         $scope.getIntroClass = this.getIntroClass.bind(this);
         $scope.getIntroInnerClass = this.getIntroInnerClass.bind(this);
 
-        $(window).on('scroll', _.debounce(() => {
-            $scope.$apply(() => {
-                var passed = $(window).scrollTop() + ($(window).innerHeight() * 0.90) >
-                             $('.header__intro').offset().top + $('.header__intro').outerHeight();
-                if (!this.folded && passed) {
-                    $('.header__intro__inner')
-                        .css('max-height', $('.header__intro__inner').outerHeight());
-                    this.folded = true;
-                    $('.header__intro__inner').css('max-height', '');
-                } else if (this.folded && $(window).scrollTop() <= 0) {
-                    this.folded = false;
-                }
-            });
-        }, 100));
+        // $(window).on('scroll', _.debounce(() => {
+        //     $scope.$apply(() => {
+        //         var passed = $(window).scrollTop() + ($(window).innerHeight() * 0.90) >
+        //                      $('.header__intro').offset().top + $('.header__intro').outerHeight();
+        //         if (!this.folded && passed) {
+        //             $('.header__intro__inner')
+        //                 .css('max-height', $('.header__intro__inner').outerHeight());
+        //             this.folded = true;
+        //             $('.header__intro__inner').css('max-height', '');
+        //         } else if (this.folded && $(window).scrollTop() <= 0) {
+        //             this.folded = false;
+        //         }
+        //     });
+        // }, 100));
     }
 
     getIntroClass () {
