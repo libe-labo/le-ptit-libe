3b02bcaae24fccf8deaa42d9ae4d3167ad0bc318
Paul Joannon <hello@pauljoannon.com>
2016-04-27 11:00:43 +0200

    footer menu w/ flexbox

--- src/app/components/menu/menu.controller.js
+++ src/app/components/menu/menu.controller.js
@@ -7,7 +7,6 @@ class MenuController {
         this.$stateParams = $stateParams;
 
         $scope.getVignetteRepetition = this.getVignetteRepetition.bind(this);
-        $scope.getVignetteArticleId = this.getVignetteArticleId.bind(this);
         $scope.getVignetteImage = this.getVignetteImage.bind(this);
 
         $rootScope.$on('$viewContentLoaded', function() {
@@ -17,13 +16,6 @@ class MenuController {
 
     getVignetteRepetition () {
         return _.range(0, 7);
-    }
-
-    getVignetteArticleId (row, col) {
-        if (row <= 0) {
-            return 0;
-        }
-        return 1 + ((row - 1) * 3) + col;
     }
 
     getVignetteImage (idx) {
